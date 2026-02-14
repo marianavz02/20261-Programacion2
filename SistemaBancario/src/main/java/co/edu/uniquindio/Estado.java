1cd3578c1d0a38052bc90c1768d89fcd8b1a4518
marianavz02 <mariana.vasquezz@uqvirtual.edu.co>
2026-02-13 22:03:47 -0500

    Delete SistemaBancario directory

--- SistemaBancario/src/main/java/co/edu/uniquindio/Estado.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio;
-
-public enum Estado {
-    ACTIVA, BLOQUEADA;
-}
--- SistemaBancario/src/main/java/co/edu/uniquindio/TipoMovimiento.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio;
-
-public enum TipoMovimiento {
-    DEPOSITAR, RETIRAR, BLOQUEAR, DESBLOQUEAR;
-}
